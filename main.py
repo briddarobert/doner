876a65750e6bcd7fb9c1e31310a00d2b7a426fe6
Robert Bridda <briddarobert@icloud.com>
2024-10-26 19:23:38 +0200

    Clean up

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 """ OpenCV, for image processing """
 import cv2
 
+FRAME_WIDTH = 1280
+FRAME_HEIGHT = 720
+
 ON_TRACK_BUFFER = 200
 
 # Setup the camera
 video_capture = cv2.VideoCapture(0)
-video_capture.set(3, 1280)
-video_capture.set(4, 720)
-
-width = video_capture.get(cv2.CAP_PROP_FRAME_WIDTH)
-height = video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+video_capture.set(3, FRAME_WIDTH)
+video_capture.set(4, FRAME_HEIGHT)
 
 while True:
     # Capture a frame
     ret, frame = video_capture.read()
     # Crop the image
     # [startY:endY, startX:endX]
-    crop_img = frame[0:720, 0+100:1280-100]
+    crop_img = frame[0:FRAME_HEIGHT, 0+100:FRAME_WIDTH-100]
     # Convert to grayscale
     gray = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
     # Apply Gaussian blur to reduce noise
@@ -33,14 +33,14 @@ while True:
         cx = int(M['m10']/M['m00'])
         cy = int(M['m01']/M['m00'])
         print(str(cx) + " " + str(cy))
-        cv2.line(crop_img, (cx, 0), (cx, 720), (255, 0, 0), 1)
-        cv2.line(crop_img, (0, cy), (1280, cy), (255, 0, 0), 1)
+        cv2.line(crop_img, (cx, 0), (cx, FRAME_HEIGHT), (255, 0, 0), 1)
+        cv2.line(crop_img, (0, cy), (FRAME_WIDTH, cy), (255, 0, 0), 1)
         cv2.drawContours(crop_img, contours, -1, (0, 255, 0), 1)
-        if cx >= (1280-100)/2 + ON_TRACK_BUFFER:
+        if cx >= (FRAME_WIDTH-100)/2 + ON_TRACK_BUFFER:
             print("Turn Left!")
-        if cx < (1280-100)/2 + ON_TRACK_BUFFER and cx > (1280-100)/2 - ON_TRACK_BUFFER:
+        if cx < (FRAME_WIDTH-100)/2 + ON_TRACK_BUFFER and cx > (FRAME_WIDTH-100)/2 - ON_TRACK_BUFFER:
             print("On Track!")
-        if cx <= (1280-100)/2 - ON_TRACK_BUFFER:
+        if cx <= (FRAME_WIDTH-100)/2 - ON_TRACK_BUFFER:
             print("Turn Right")
     else:
         print("I don't see the line")
