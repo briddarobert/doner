b27525b7d4a9bc60179c9271e91ac4c850edc573
Robert Bridda <briddarobert+github@icloud.com>
2024-12-12 15:14:54 +0100

    Add cool line

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 """ OpenCV, for image processing """
 import cv2
+""" numpy for maths stuff """
+import numpy as np
+from cv2_enumerate_cameras import enumerate_cameras
+
+for camera_info in enumerate_cameras():
+    print(f'{camera_info.index}: {camera_info.name}')
 
 FRAME_WIDTH = 1280
 FRAME_HEIGHT = 720
@@ -7,7 +13,7 @@ FRAME_HEIGHT = 720
 ON_TRACK_BUFFER = 200
 
 # Setup the camera
-video_capture = cv2.VideoCapture(0)
+video_capture = cv2.VideoCapture(204)
 video_capture.set(3, FRAME_WIDTH)
 video_capture.set(4, FRAME_HEIGHT)
 
@@ -27,8 +33,24 @@ while True:
     contours, hierarchy = cv2.findContours(
         thresh.copy(), 1, cv2.CHAIN_APPROX_NONE)
     # Find the biggest contour (if detected)
+    # Draw turning area
+    cv2.line(crop_img, ((FRAME_WIDTH-100)//2 - ON_TRACK_BUFFER, 0), ((FRAME_WIDTH-100)//2 - ON_TRACK_BUFFER, FRAME_HEIGHT), (0, 0, 255), 1)
+    cv2.line(crop_img, ((FRAME_WIDTH-100)//2 + ON_TRACK_BUFFER, 0), ((FRAME_WIDTH-100)//2 + ON_TRACK_BUFFER, FRAME_HEIGHT), (0, 0, 255), 1)
     if len(contours) > 0:
         c = max(contours, key=cv2.contourArea)
+        rows,cols = crop_img.shape[:2]
+        [vx,vy,x,y] = cv2.fitLine(c, cv2.DIST_L2,0,0.01,0.01)
+        lefty = int((-x*vy/vx) + y)
+        righty = int(((cols-x)*vy/vx)+y)
+        cv2.line(crop_img,(cols-1,righty),(0,lefty),(0,255,0),2)
+        if (lefty > 0 and righty > 0):
+            if (lefty > righty):
+                print("Left turn ahead")
+            elif (righty > lefty):
+                print("Left turn ahead")
+            else:
+                print("Wut?! (forse incrocio)")
+        print(lefty, righty)
         M = cv2.moments(c)
         cx = int(M['m10']/M['m00'])
         cy = int(M['m01']/M['m00'])
@@ -37,13 +59,13 @@ while True:
         cv2.line(crop_img, (0, cy), (FRAME_WIDTH, cy), (255, 0, 0), 1)
         cv2.drawContours(crop_img, contours, -1, (0, 255, 0), 1)
         if cx >= (FRAME_WIDTH-100)/2 + ON_TRACK_BUFFER:
-            print("Turn Left!")
+            print("Turn right! Adjustment")
         if cx < (FRAME_WIDTH-100)/2 + ON_TRACK_BUFFER and cx > (FRAME_WIDTH-100)/2 - ON_TRACK_BUFFER:
-            print("On Track!")
+            print("On track!")
         if cx <= (FRAME_WIDTH-100)/2 - ON_TRACK_BUFFER:
-            print("Turn Right")
+            print("Turn left! Adjustment")
     else:
-        print("I don't see the line")
+        print("No line found")
 
     # Display the resulting frame
     cv2.imshow('frame', crop_img)
